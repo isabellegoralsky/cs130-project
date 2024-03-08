9a6a61f7e7be7baab0192b174979dce9f8d5fd54
Clement Nguyen <nguyenclementp@gmail.com>
2024-03-07 18:35:43 -0800

    Revert "Goals schema and add/delete/get endpoints"

--- index.js
+++ index.js
@@ -33,9 +33,6 @@ app.use('/profile', profileRoute);
 const personalRecordRoute = require('./routes/personalRecordRoute');
 app.use('/personalrecord', personalRecordRoute);
 
-const goalRoute = require('./routes/goalRoute');
-app.use('/goal', goalRoute);
-
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
