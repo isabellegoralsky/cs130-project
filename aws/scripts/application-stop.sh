076ef5bcb54d1062685a8c6ae26aac6e923b46a5
Saarthak Sharma <sharma.saarthak@gmail.com>
2024-03-10 15:53:19 -0700

    fixing bug with application stop, running with sudo

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -5,7 +5,7 @@ set -x
 # Check if port 8080 is being listened to
 if sudo lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null; then
     # If a program is listening on port 8080, kill it
-    sudo lsof -Pi :8080 -sTCP:LISTEN -t | xargs kill -9
+    sudo lsof -Pi :8080 -sTCP:LISTEN -t | xargs sudo kill -9
     echo "Program listening on port 8080 has been killed."
 else
     echo "No program is listening on port 8080."
@@ -13,7 +13,7 @@ fi
 
 if sudo lsof -Pi :3001 -sTCP:LISTEN -t >/dev/null; then
     # If a program is listening on port 8080, kill it
-    sudo lsof -Pi :30001 -sTCP:LISTEN -t | xargs kill -9
+    sudo lsof -Pi :3001 -sTCP:LISTEN -t | xargs sudo kill -9
     echo "Program listening on port 3001 has been killed."
 else
     echo "No program is listening on port 3001."
